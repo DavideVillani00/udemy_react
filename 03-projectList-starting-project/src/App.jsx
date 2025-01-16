6ca83655d96d254beb2f4453b774276975c497b6
DavideVillani00 <villani.dado@gmail.com>
2025-01-16 23:44:08 +0100

    iniziato progetto

---
+++ 03-projectList-starting-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Aside from "./components/Aside.jsx";
+import ProjectSection from "./components/ProjectSection.jsx";
+function App() {
+  return (
+    <main className="flex">
+      <Aside/>
+      <ProjectSection/>
+    </main>
+  );
+}
+
+export default App;
---
+++ 03-projectList-starting-project/src/components/Aside.jsx
@@ -0,0 +1,12 @@
+export default function Aside(){
+
+    return(
+        <aside className="container items-start gap-10 bg-black px-12 py-20 h-screen w-4/12 rounded-r-3xl">
+            <h1  className="h1 text-white">Your project</h1>
+            <button className="btn">+ Add project</button>
+            <ol>
+                <li>progetti</li>
+            </ol>
+        </aside>
+    )
+}
